de7fc9375231f3959121e0b1c3711edf6c523b45
Sean Safari <safaris1@tcnj.edu>
2013-02-22 00:15:17 -0500

    small comments

--- Pushpin/src/com/cloud/pushpin/MainActivity.java
+++ Pushpin/src/com/cloud/pushpin/MainActivity.java
@@ -80,19 +80,20 @@ public class MainActivity extends FragmentActivity {
 			mMap.addMarker(mopt);
 			
 			}
-
+		
+		//not used
 		@Override
 		public void onProviderDisabled(String arg0) {
 			// TODO Auto-generated method stub
 
 		}
-
+		//not used
 		@Override
 		public void onProviderEnabled(String arg0) {
 			// TODO Auto-generated method stub
 
 		}
-
+		//not used
 		@Override
 		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
 			// TODO Auto-generated method stub
