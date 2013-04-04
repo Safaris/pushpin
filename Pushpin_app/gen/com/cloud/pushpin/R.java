f2bb6fa2a060b3d82740a398bcf74db26d0d7ac3
dr4g1116 <dragm1@tcnj.edu>
2013-04-04 11:07:13 -0400

    Re-added the aboutus XML
    Looks good now.

--- Pushpin_app/gen/com/cloud/pushpin/R.java
+++ Pushpin_app/gen/com/cloud/pushpin/R.java
@@ -148,7 +148,7 @@ containing a value of this type.
         public static final int button1=0x7f04000c;
         public static final int button2=0x7f04000d;
         public static final int map=0x7f040008;
-        public static final int menu_settings=0x7f04000e;
+        public static final int menu_settings=0x7f040012;
         public static final int message=0x7f040004;
         public static final int message1=0x7f040005;
         public static final int message2=0x7f040006;
@@ -159,6 +159,10 @@ containing a value of this type.
         public static final int terrain=0x7f040003;
         public static final int textView1=0x7f040009;
         public static final int textView2=0x7f04000a;
+        public static final int textView3=0x7f04000e;
+        public static final int textView4=0x7f04000f;
+        public static final int textView5=0x7f040010;
+        public static final int textView6=0x7f040011;
     }
     public static final class layout {
         public static final int accountinfo=0x7f030000;
