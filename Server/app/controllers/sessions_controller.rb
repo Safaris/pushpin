79f507f50ce03b27e3a9eaeef566478029d25343
Eric Palace <epalace510@gmail.com>
2013-04-12 00:12:08 -0400

    Changed a file name and finished up the authenticity management.

--- Server/app/controllers/sessions_controller.rb
+++ Server/app/controllers/sessions_controller.rb
@@ -5,14 +5,12 @@ class SessionsController < ApplicationController
     user=User.find_by_username(params[:username])
     if(user && user.authenticate(params[:password]))
       #Create session, store logged in ID
-      session[:user_id]=user.id
-      render json: {logged_in: true, session_key: session[:user_id]}
+      render json: {logged_in: true, access_token: @user.api_key.auth_token}
     else
-      render json: {logged_in: false, session_key: nil}
+      render json: {logged_in: false, access_token: nil}
     end
   end
   def destroy
-    session[:user_id]=nil
     render json: {logged_out: true}
     #redirect_to root_path, :notice => 'Logged out.'
   end
