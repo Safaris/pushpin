a517393100e525f0de0bc5971d8da7a0bd18540f
Eddie Spencer <spencee1@tcnj.edu>
2013-04-11 21:15:46 -0400

    Updated User Model

--- PushPin/app/models/user.rb
+++ PushPin/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
    attr_accessible :password, :username, :lat, :long
+   has_many :client_applications
+   has_many :tokens, :class_name=>"OauthToken",:order=>"authorized_at desc",:include=>[:client_application]
    has_secure_password
 end
