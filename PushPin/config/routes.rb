fa28c29266674d054f1ef41c7e5f9c89076987b3
Eddie Spencer <spencee1@tcnj.edu>
2013-04-11 21:09:01 -0400

    Added oauth framework

--- PushPin/config/routes.rb
+++ PushPin/config/routes.rb
@@ -1,4 +1,18 @@
 PushPin::Application.routes.draw do
+  resources :oauth_clients
+
+  match '/oauth/test_request',  :to => 'oauth#test_request',  :as => :test_request
+
+  match '/oauth/token',         :to => 'oauth#token',         :as => :token
+
+  match '/oauth/access_token',  :to => 'oauth#access_token',  :as => :access_token
+
+  match '/oauth/request_token', :to => 'oauth#request_token', :as => :request_token
+
+  match '/oauth/authorize',     :to => 'oauth#authorize',     :as => :authorize
+
+  match '/oauth',               :to => 'oauth#index',         :as => :oauth
+
   post '/' => 'users#index', :defaults => {:format => 'json'}
   resources :users , :defaults => {:format => 'json'}
   get 'logout' => 'sessions#destroy', :as => 'logout'
